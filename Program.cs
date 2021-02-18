d772948d80b76f198fb88d22203e3b55f2ff984e
Jesús Beleño <persomultimate@gmail.com>
2021-02-18 16:04:21 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program2
+
+    {
+        static void Main(string[] args)
+        {
+            int NumeroUno = 0; int NumeroDos = 0; int suma = 0;
+
+            Console.Write("digite el PRIMER numero ");
+            NumeroUno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("digite el SEGUNDO numero");
+            NumeroDos = Convert.ToInt32(Console.ReadLine());
+            suma = NumeroUno + NumeroDos;
+
+            Console.WriteLine($"La suma de los numeros es {suma}");
+            Console.ReadKey();
+
+        }
+    }
+
+}
